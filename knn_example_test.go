af14853eaf9910b5e271b92ba2f7c174ab026a0c
Andrea Masi <eraclitux@gmail.com>
2017-04-25 11:57:37 +0200

    Add license

--- knn_example_test.go
+++ knn_example_test.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2017 Andrea Masi. All rights reserved.
+// Use of this source code is governed by MIT license
+// which that can be found in the LICENSE.txt file.
+
 package learn_test
 
 import (
--- validation.go
+++ validation.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2015 Andrea Masi. All rights reserved.
+// Copyright (c) 2017 Andrea Masi. All rights reserved.
 // Use of this source code is governed by MIT license
 // which that can be found in the LICENSE.txt file.
 
